d53a4e8301224d27d12807c0dbd10ba8f7ba3d54
Anelya Dusaeva <an.dusaeva+git@gmail.com>
2023-03-14 01:54:22 +0300

    updated lexer tests to new lexer output format

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -4,65 +4,65 @@ from src.Lexer import *
 
 def test_quotes():
     command = "echo 'hello'"
-    right_expr = [StrLex("echo"), Space(), Quoted("hello"), EndLine()]
+    right_expr = [StrLex("echo"), Space(), Quoted("hello"), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_doublequotes():
     command = "something \"hey this is quoted string!\" \"and one more\""
     right_expr = [StrLex("something"), Space(), DoubleQuoted("hey this is quoted string!"),
-                  Space(), DoubleQuoted("and one more"), EndLine()]
+                  Space(), DoubleQuoted("and one more"), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_quotes_without_spacing():
     command = "bop bop 'bip''bip'"
     right_expr = [StrLex("bop"), Space(), StrLex('bop'), Space(),
-                  Quoted('bip'), Quoted('bip'), EndLine()]
+                  Quoted('bip'), Quoted('bip'), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_different_quotes_without_spacing():
     command = "bop bop \"bip\"'bip'"
     right_expr = [StrLex("bop"), Space(), StrLex('bop'), Space(),
-                  DoubleQuoted('bip'), Quoted('bip'), EndLine()]
+                  DoubleQuoted('bip'), Quoted('bip'), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_double_quotes_without_spacing():
     command = "bop bop \"bip\"\"bip\""
     right_expr = [StrLex("bop"), Space(), StrLex('bop'), Space(),
-                  DoubleQuoted('bip'), DoubleQuoted('bip'), EndLine()]
+                  DoubleQuoted('bip'), DoubleQuoted('bip'), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_quotes_in_double_quotes():
     command = "bop bop \"b'i'p\"\"bip\""
     right_expr = [StrLex("bop"), Space(), StrLex('bop'), Space(),
-                  DoubleQuoted('b\'i\'p'), DoubleQuoted('bip'), EndLine()]
+                  DoubleQuoted('b\'i\'p'), DoubleQuoted('bip'), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 def test_double_quotes_in_quotes():
     command = "bop bop 'b\"i\"p'"
     right_expr = [StrLex("bop"), Space(), StrLex('bop'), Space(),
-                  Quoted('b"i"p'), EndLine()]
+                  Quoted('b"i"p'), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_pipe():
     command = "asd| zxc"
-    right_expr = [StrLex("asd"), PipeChar(), Space(), StrLex("zxc"), EndLine()]
+    right_expr = [StrLex("asd"), PipeChar(), Space(), StrLex("zxc"), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_equals():
     command = "asd=123"
-    right_expr = [StrLex("asd"), Equal(), StrLex("123"), EndLine()]
+    right_expr = [StrLex("asd"), Equal(), StrLex("123"), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
 
 def test_spacing():
     command = "asd  asd"
-    right_expr = [StrLex("asd"), Space(), StrLex("asd"), EndLine()]
+    right_expr = [StrLex("asd"), Space(), StrLex("asd"), EndLine(), EndOfFile()]
     lexer = Lexer(command)
     assert list(lexer.get()) == right_expr
